fbb668511cec3afbf245021a04fcd19e379c60f1
dbejo <david.bejo@genesys.com>
2023-03-25 20:32:11 +0100

    Add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { EventComponent } from './event/event/event.component';
+import { PersonComponent } from './person/person/person.component';
+import { OrganizationComponent } from './organization/organization/organization.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, EventComponent, PersonComponent, OrganizationComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
