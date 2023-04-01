07f6b4f9ca7d3f7223156485a6c16258a8604308
dbejo <bejodavid123@gmail.com>
2023-04-02 01:22:41 +0200

    finalize delete implementation

--- src/app/person/component/person.component.ts
+++ src/app/person/component/person.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { Person } from '../person';
+import { PersonService } from '../service/person.service';
+import { CustomResponse } from 'src/app/custom-response/custom-response';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-person',
@@ -9,17 +10,40 @@ import { Person } from '../person';
   styleUrls: ['./person.component.css']
 })
 export class PersonComponent implements OnInit {
-  private API_URL = environment.apiUrl;
-  people?: Person[];
+  people: Person[];
   detailsToShow: number[] = [];
-  constructor(private http: HttpClient) {}
+  isHidden = false;
+  hoveredItem: number | null = null;
+
+  constructor(private personService: PersonService) {}
 
   ngOnInit(): void {
     this.getPeople();
   }
 
-  getPeople() {
-    return this.http.get<Person[]>(this.API_URL + "/api/v1/person/all").subscribe((data) => this.people = data)
+  public getPeople(): void {
+    this.personService.getPeople().subscribe(
+      (response: CustomResponse) => {
+        this.people = response.data.people;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  public deletePerson(person: Person): void {
+    if(confirm(`You are about to delete ${person.firstName} ${person.lastName}. Do you wish to proceed?`)) {
+      this.personService.deletePerson(person.id).subscribe(
+        (response: void) => {
+          console.log(response);
+          this.getPeople();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message)
+        }
+      );
+      }
   }
 
   itemClickHandler(i: number) {
--- src/app/person/service/person.service.ts
+++ src/app/person/service/person.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CustomResponse } from 'src/app/custom-response/custom-response';
+import { environment } from 'src/environments/environment';
+import { Event } from 'src/app/event/event'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
+  private API_URL = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPeople(): Observable<CustomResponse> {
+    return this.http.get<CustomResponse>(`${this.API_URL}/api/v1/person/all`);
+  }
+
+  deletePerson(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/api/v1/person/${id}`);
+  }
+
+  getPersonEvents(id: number): Observable<CustomResponse> {
+    return this.http.get<CustomResponse>(`${this.API_URL}/api/v1/person/${id}/events`);
+  }
 }
