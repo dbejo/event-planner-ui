32e42003d61d2884bd17abe33eeb6bb1aba019d5
dbejo <bejodavid123@gmail.com>
2023-04-05 20:33:14 +0200

    List real organizations

--- src/app/person/modal/component/person-modal/person-modal.component.ts
+++ src/app/person/modal/component/person-modal/person-modal.component.ts
@@ -1,10 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { CustomResponse } from 'src/app/custom-response/custom-response';
+import { Organization } from 'src/app/organization/organization';
+import { OrganizationService } from 'src/app/organization/service/organization.service';
 
 @Component({
   selector: 'app-person-modal',
   templateUrl: './person-modal.component.html',
   styleUrls: ['./person-modal.component.css']
 })
-export class PersonModalComponent {
+export class PersonModalComponent implements OnInit {
   @Input() title!: string;
+  public organizations: Organization[];
+  constructor(private organizationService: OrganizationService) {}
+  ngOnInit(): void {
+    this.getOrganizations();
+  }
+  
+  public getOrganizations(): void {
+    this.organizationService.getOrganizations().subscribe(
+      (response: CustomResponse) => {
+        this.organizations = response.data.organizations;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
 }
