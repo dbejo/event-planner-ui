6eade4ea29df67b861b5d97cd6721601a14fa62d
dbejo <bejodavid123@gmail.com>
2023-04-17 19:52:34 +0200

    Move login to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,8 @@
 import { Component } from '@angular/core';
-import { GoogleApiService, UserInfo } from './google-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-oauth2-oidc';
-
-  userInfo?: UserInfo
-
-  constructor(private readonly googleApi: GoogleApiService) { 
-    googleApi.userProfileSubject.subscribe(info => {
-      this.userInfo = info
-    })
-  }
-
-  isLoggedIn(): boolean {
-    return this.googleApi.isLoggedIn()
-  }
-
-  logout() {
-    this.googleApi.signOut()
-  }
-
-}
+export class AppComponent {}
--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GoogleApiService, UserInfo } from 'src/app/google-api.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  userInfo: UserInfo;
 
+  constructor(private readonly googleApi: GoogleApiService) {
+    googleApi.userProfileSubject.subscribe((info) => {
+      this.userInfo = info;
+    });
+  }
+
+  isLoggedIn(): boolean {
+    return this.googleApi.isLoggedIn();
+  }
+
+  logout() {
+    this.googleApi.signOut();
+  }
 }
